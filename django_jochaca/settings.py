e3ea1406bd1c7073ae79c10541115e259f7432d0
jochaca <johncharlie.caburnay@dbtc-cebu.edu.ph>
2022-05-21 10:55:42 +0800

    project updated

--- django_jochaca/settings.py
+++ django_jochaca/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 import os
 import environ
 from pathlib import Path
@@ -136,3 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'todolist'
 LOGIN_URL='login'
+
+django_heroku.settings(locals())
